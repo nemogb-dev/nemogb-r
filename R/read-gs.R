1e133b6d948e0cd438d91febc3fc0ab00ee121d2
Zach Turner <zachturner@berkeley.edu>
2024-10-22 20:13:41 -0700

    works

--- R/read-gs.R
+++ R/read-gs.R
@@ -25,6 +25,26 @@ read_gs <- function(path, verbose = FALSE){
 
 
 
+#' This function takes in gradebook csv and lateness csv from Canvas
+#' and aggregates them into identical format as those from Gradescope. 
+#' Note that Canvas defaults assignment due time to beginning of day in lateness csv
+#' regardless of true time of lateness. 
+#'
+#'Data is then checked for correct format.
+#'
+#'@param path_to_grades The path to Canvas gradebook csv
+#'@param path_to_lateness The path to Canvas csv for assignment Lateness
+#'
+#'@return A tibble in correct format a la Gradescope
+#'
+#'
+#'@importFrom readr read_csv
+#'@importFrom  dplyr rename rename_with select left_join slice
+#'@importFrom purrr discard
+#'@importFrom stringr str_extract str_remove str_match
+#'@importFrom hms hms
+#'@importFrom lubridate as_datetime
+#'@importFrom tidyr pivot_wider
 read_canvas <- function(path_to_grades, path_to_lateness, verbose = FALSE){
   
   #read in grade data
@@ -44,21 +64,25 @@ read_canvas <- function(path_to_grades, path_to_lateness, verbose = FALSE){
   #assignments are columns that end in (xxxx) with xxx being some number
   
   assignments <- stringr::str_extract(names(grades),
-                                          ".+\\s*\\(\\d+\\)")
+                                          ".+\\s*\\(\\d+\\)") 
+  assignments <- purrr::discard(assignments, is.na)
   
   #remove assignment identifiers
-  assignments_short <- stringr::str_extract(assignments, "[^\\s*(\\d+)]+")
+  assignments_short <- stringr::str_remove(assignments, "\\s\\(\\d+\\)")
   
-  if (duplicated(assignments_short)){
-    stop("Duplicated Assignment Names")
+  if (any(duplicated(assignments_short))){
+   stop("Duplicated Assignment Names")
   }
   
   #rename to shorter name for clarity and consistency with gradescope
   grades <- grades |>
     dplyr::rename_with(function(x) {
-      stringr::str_extract(x, "[^\\s*(\\d+)]+")
+      stringr::str_remove(x, "\\s\\(\\d+\\)")
     },  
-      assignments)
+      assignments) |>
+    dplyr::select(any_of(c(assignments_short, "Student", "ID", 
+                    "SIS User ID", "SIS Login ID", "Section"))) |>
+    dplyr::rename(Sections = Section)
   
   #then convert lateness into proper form
   
@@ -69,7 +93,7 @@ read_canvas <- function(path_to_grades, path_to_lateness, verbose = FALSE){
                                                      format = "%b %d, %Y")
   
   #find lateness in hms format to be consistent
-  lateness_data$`Lateness (H:M:S)` <- hms::hms(hours = as.double(
+  lateness_data$`Lateness (H:M:S)` <- lubridate::hms(hours = as.double(
     difftime(lateness_data$`Submitted Date` ,
              lateness_data$`Due Date`)
   ))
@@ -85,8 +109,9 @@ read_canvas <- function(path_to_grades, path_to_lateness, verbose = FALSE){
         dplyr::rename(`Submission Time` = `Submitted Date`) |>
         tidyr::pivot_wider(names_from = `Assignment Name`,
                            values_from = c(`Lateness (H:M:S)`, `Submission Time`),
-                           names_sep = " - ",
-                           values_fill = hms::hms(hours = 0)
+                           names_glue = "{`Assignment Name`} - {.value}",
+                           values_fill = list(`Lateness (H:M:S)` = lubridate::hms(hours = 0),
+                                              `Submission Time` = lubridate::as_datetime(0))
                            
                                            )
   
@@ -105,7 +130,7 @@ read_canvas <- function(path_to_grades, path_to_lateness, verbose = FALSE){
   unseen_lateness <- paste0(unseen_late_assignments , 
                             " - Lateness (H:M:S)")
   #not late at all
-  lateness_data[unseen_lateness] <- hms::hms(hours = 0)
+  lateness_data[unseen_lateness] <- lubridate::hms(hours = 0)
   
   
   #now merge tables
@@ -127,12 +152,41 @@ read_canvas <- function(path_to_grades, path_to_lateness, verbose = FALSE){
   
   grades[sub_cols][is.na(grades[sub_cols])] <- lubridate::as_datetime(0)
   
-  grades[late_cols][is.na(grades[late_cols])] <- hms::hms(hours = 0)
+  grades[late_cols][is.na(grades[late_cols])] <- lubridate::hms(hours = 0)
   
   #now we have handled lateness 
   #time to turn to dealing with other things
   
   
+  #now handle max points
+  
+  max_points <- dplyr::filter(grades, Student == "    Points Possible") |>
+    dplyr::select(all_of(assignments_short)) |>
+    dplyr::slice(rep.int(1, nrow(grades) - 1)) |>
+    dplyr::rename_with(function(x){
+      paste0(x, " - Max Points")
+    })
+  
+  
+  grades <- dplyr::filter(grades, Student != "    Points Possible")
+  
+  #now add max points columns
+  max_points$ID <- grades$ID
+  grades <- dplyr::left_join(grades, max_points, 
+                             by = dplyr::join_by(ID == ID))
+  
+  #now convert names to other style
+  
+  grades$`First Name` <- (stringr::str_match(grades$Student, 
+                                            ".+,(.+)"))[, 2]
+  grades$`Last Name` <- (stringr::str_match(grades$Student, 
+                                            "(.+),.+"))[, 2]
+  
+  grades <- grades |>
+    dplyr::select(-c("Student", "ID", "SIS Login ID")) |>
+    dplyr::rename(SID = `SIS User ID`)
+  
+  check_data_format(grades)
   
   
   
