631584764b8a88f2e259a38e0fd9dbbb79889428
nikita-jaya <nikita.jp@gmail.com>
2025-04-15 09:25:11 -0700

    rough outline of functions

--- R/exceptions.R
+++ R/exceptions.R
@@ -2,5 +2,58 @@ calculate_grades_with_exceptions <- function(gs = gs, policy = policy){
   # if no exceptions
   if (is.null(policy$exceptions) | length(policy$exceptions) == 0){
     calculate_grades(gs, policy)
+  } else{
+    policy |>
+      separate_policies() |>
+      calculate_grades_with_exceptions()
   }
+}
+
+separate_policies <- function(policy){
+  policies <- list(policy = list(policy), gs = list(gs))
+  for (exception in policy$exceptions){
+    # find indices of exception category in full policy
+    index <- find_indices(policy$categories, exception$category)
+    # if exception applies to existing category
+    if (length(index) > 0) {
+      # add truncated gs
+      policies$gs <- append(policies$gs, list(filter(gs, SID %in% exception$SID)))
+      #remove exception students from initial gs
+      policies[["gs"]][[1]] <-filter(gs, !(SID %in% exception$SID))
+      #add policy with exception to policy
+      exception$SID <- NULL
+      index_cmd <- paste0("policy$categories[[", paste(index, collapse = "]]$assignments[["), "]] <- exception")
+      eval(parse(text = index_cmd))
+    } else {
+      warning("Exception is creating a new category")
+    }
+  }
+  return (policies)
+}
+
+calculate_grades_with_exceptions <- function(policies, gs){
+  purrr::map2(policies$gs, policies$policy, calculate_grades)
+}
+
+find_indices <- function(lst, target, current_index = c()) {
+  indices <- c()
+  
+  for (i in seq_along(lst)) {
+    if (is.list(lst[[i]]) && !is.null(lst[[i]]$category) && identical(lst[[i]]$category, target)) {
+      indices <- c(current_index, i)
+      break
+    } else if (is.list(lst[[i]]) && !is.null(lst[[i]]$assignments)) {
+      indices <- find_indices(lst[[i]]$assignments, target, c(current_index, i))
+    }
+    
+    if (length(indices) > 0) {
+      break
+    }
+  }
+  
+  if (length(indices) == 0) {
+    return(NULL)
+  }
+  
+  return(indices)
 }
--- tests/testthat/test-exceptions.R
+++ tests/testthat/test-exceptions.R
@@ -1,4 +1,48 @@
 test_that("one exception - extra quiz drop", {
+  categories <- list(
+    category = "Overall Grade",
+    aggregation = "weighted_mean",
+    assignments = list(
+      list(
+        category = "Labs",
+        weight = 0.5,
+        aggregation = "equally_weighted",
+        assignments = list(
+          list(
+            category = "Lab 1",
+            aggregation = "weighted_by_points",
+            assignments = c("Lab 1.1", "Lab 1.2")
+          ),
+          list(
+            category = "Lab 2",
+            aggregation = "weighted_by_points",
+            assignments = c("Lab 2.1", "Lab 2.2", "Lab 2.3")
+          )
+        )
+      ),
+      list(
+        category = "Quiz",
+        weight = 0.5,
+        drop_n_lowest = 1,
+        aggregation = "weighted_by_points",
+        assignments = c("Quiz 1", "Quiz 2", "Quiz 3")
+      )
+    )
+  )
+  exceptions <- list(
+    list(
+      category = "Quiz",
+      weight = 0.5,
+      drop_n_lowest = 2,
+      SID = c(111, 222),
+      aggregation = "weighted_by_points",
+      assignments = c("Quiz 1", "Quiz 2", "Quiz 3")
+    )
+  )
+  policy <- list(categories = list(categories), exceptions = exceptions)
+})
+
+test_that("one exception - adding new category to overall grade", {
   expect_equal(2 * 2, 4)
 })
 
